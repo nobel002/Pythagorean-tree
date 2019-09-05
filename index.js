3b407088df2a7623f683c4df47d2e3917eed0fba
nobel002 <43477230+nobel002@users.noreply.github.com>
2019-09-05 19:11:45 +0200

    Start

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
 
+function draw() {
+  background(220);
+}
+function Update(){
+  this.square = function (){
+
+  }
+  this.triangle = function(){
+
+  }
+}
